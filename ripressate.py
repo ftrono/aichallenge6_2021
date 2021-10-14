df90c36f5d215b0178ffea4654d38dc104dc95c6
ftrono <82730291+ftrono@users.noreply.github.com>
2021-10-14 19:15:54 +0200

    complete ripressate.py with dicts and lists - no classes

--- ripressate.py
+++ ripressate.py
@@ -1,23 +1,32 @@
 from utils import mongo_connect, mongo_disconnect
 import matplotlib.pyplot as plt
 db,client = mongo_connect()
-posts=db.test1
+POSTS=db.test1
 
-#Vars:
-ripressate = {}
-dups = []
-prior = ''
-new = ''
-count = 1
-i = 0
-limit = 1
-height = []
-force = []
-series_height = {}
-series_force = {}
+def find_masters(POSTS):
+    tags = POSTS.distinct("master")
 
-#Loop across posts:
-for post in posts.find():
+    for tag in tags:
+        print(tag)
+
+    #return tags
+
+def find_duplicates(post):
+
+    #Vars:
+    ripressate = {}
+    dups = []
+    prior = ''
+    new = ''
+    count = 1
+    height = []
+    force = []
+    series = {}
+    newread = []
+    priorread = []
+    totread = []
+    i = 0
+    last = len(post["steps"])
 
     #Check every step in the post:
     for step in post["steps"]:
@@ -32,43 +41,84 @@ for post in posts.find():
                 force.append(s2)
             except:
                 print("Line skipped for ID: {}".format(new))
+            
+        #save to buffer:
+        newread = [height, force]
+        height = []
+        force = []
 
         #Duplicate match:
         if new == prior:
+            #append buffer from prior read:
+            totread.append(priorread)
             count = count+1
 
         else:
             #Store buffer in tuple:
             if count > 1:
-                #save to dict:
-                series_height[count] = height[:-1]
-                series_force[count] = force[:-1]
-                print(series_height[prior])
-                print(series_force[prior])
                 #Store to tuple:
                 tup = (prior, count)
                 dups.append(tup)
+                #save to dict of pressate by id:
+                totread.append(priorread)
+                series[prior] = totread
+                #reset count:
                 count = 1
+                totread = []
 
-            #keep only last read:
-            height = [height[-1]]
-            force = [force[-1]]
+        #If last component of the riduttore and buffer is not empty, store:
+        if i == last and count > 1:
+            #Store to tuple:
+            tup = (prior, count)
+            dups.append(tup)
+            #save to dict of pressate by id:
+            totread.append(priorread)
+            series[prior] = totread
+            #reset count:
+            count = 1
+            totread = []
 
         #Reset:
         prior = new
-    
-    #Store tuple in overall dictionary id:tuple
-    ripressate[str(post["_id"])] = dups
-    dups = []
-    prior = ''
+        priorread = newread
+        newread = []
+        i = i+1
+        
+        #Store tuple in overall dictionary id:tuple
+        ripressate[str(post["_id"])] = dups
+        #newread = []
+
+    return ripressate, series
+
+
+#MAIN:
+i = 0
+limit = 1
+
+#tags = find_masters(POSTS)
+#print(tags)
+
+#Loop across POSTS:
+for post in POSTS.find():
+
+    ripressate, series = find_duplicates(post)
     i = i+1
 
     #Break clause:
     if i >= limit:
         break
 
+print("DUPLICATES FOUND:")
 for item in ripressate.items():
     print(item)
+    break
+
+print("\nSERIES:")
+
+for item in series.items():
+    print(item)
+    break
 
 # MOLTO IMPORTANTE ricordarsi di metterlo ogni volta che si apre una connessione
 mongo_disconnect(client)
+
