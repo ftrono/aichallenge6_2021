3fcc4d62721a6bb1b2b56a003dc49488a57575e0
giorgio289 <giorgio.scarton@studenti.unitn.it>
2021-10-27 20:58:04 +0200

    add target_vector generator

--- test.py
+++ test.py
@@ -1,15 +1,37 @@
 from utils import mongo_connect,mongo_disconnect
-from numpy import mean
-import matplotlib.pyplot as plt
+from numpy import mean,arange
+import json
 
-def mean_sample_rate(samples,max):
+def mean_sample_rate(samples):
     tmp=[]
     pos=1
-    while pos<=max:
-        tmp.append(samples[pos]-samples[pos-1])
-        pos+=1
+    if len(samples)>1: # check array lenght >1
+        while pos<len(samples):
+            if samples[pos]>0:
+                tmp.append(abs(samples[pos]-samples[pos-1]))
+            pos+=1
+    else:
+        tmp.append(0)
     return mean(tmp)
 
+def generate_vector(max_h, rate):
+    vector=[]
+    value=0
+    while value<=max_h:
+        vector.append(value)
+        value+=round(rate,2)
+    return vector
+
+
+def to_json(comp_id,taglia,max_h,rate):
+    result={
+        'comp_id' : comp_id,
+        'taglia'  : taglia,
+        'max_h'   : max_h,
+        'rate'    : rate,
+        'vector'  : generate_vector(max_h,rate)
+    }
+    return result
 
 # DB initilization
 db,client=mongo_connect()
@@ -20,30 +42,41 @@ ids=[]
 for id in result:
     ids.append(id)
 
+result= collection.find().distinct("taglia")
+tgl=[]
+for taglia in result:
+    tgl.append(taglia)
+
 #print(ids)
 
 #initialize dictionary
 sample_rates={}
 for id in ids:
-    sample_rates[id]=[]
+    sample_rates[id]={}
+    for taglia in tgl:
+        sample_rates[id][taglia]={}
+        sample_rates[id][taglia]["max_v"]=0
+        sample_rates[id][taglia]["rate"]=[]
 
-result= collection.find({},{"steps.id":1,"steps.altezza":1,"steps.forza":1})
+result= collection.find({},{"taglia":1,"steps.id":1,"steps.altezza":1,"steps.forza":1})
 
 for riduttore in result:
+    taglia_r=riduttore["taglia"]
     for pressata in riduttore["steps"]:
-        for id in ids:
-            if pressata["id"]==id:
-                max_value = max(pressata["altezza"])
-                max_index = pressata["altezza"].index(max_value)
-                if max_index==0:
-                    print(pressata["id"])
+        max_altezza = max(pressata["altezza"])
+        if max_altezza > sample_rates[id][taglia_r]["max_v"]:
+            sample_rates[pressata["id"]][taglia_r]["max_v"]=max_altezza
+        sample_rates[pressata["id"]][taglia_r]["rate"].append(mean_sample_rate(pressata["altezza"]))
 
-                sample_rates[id].append(mean_sample_rate(pressata["altezza"],max_index))
-
-for elem in sample_rates.items():
-    print(elem[0],mean(elem[1]))
+for id in ids:
+    for taglia in tgl:
+        sample_rates[id][taglia]["rate"]=mean(sample_rates[id][taglia]["rate"])
 
 
+for comp in sample_rates.keys():
+        for tg in sample_rates[comp].keys():
+            res=to_json(comp,tg,sample_rates[comp][tg]["max_v"],sample_rates[comp][tg]["rate"])
+            db.target_vectors.insert_one(res)
 
 # keep at end of query
 mongo_disconnect(client)
