ebd67c503e2d93397b082c5ed2430d985253e8c0
giorgio289 <giorgio.scarton@studenti.unitn.it>
2021-10-27 16:30:40 +0200

    removed pycahce directory and update test

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 from utils import mongo_connect,mongo_disconnect
 from numpy import mean
+import matplotlib.pyplot as plt
 
-def mean_sample_rate(samples):
+def mean_sample_rate(samples,max):
     tmp=[]
     pos=1
-    while pos<len(samples):
+    while pos<=max:
         tmp.append(samples[pos]-samples[pos-1])
         pos+=1
     return mean(tmp)
@@ -19,7 +20,7 @@ ids=[]
 for id in result:
     ids.append(id)
 
-print(ids)
+#print(ids)
 
 #initialize dictionary
 sample_rates={}
@@ -28,11 +29,16 @@ for id in ids:
 
 result= collection.find({},{"steps.id":1,"steps.altezza":1,"steps.forza":1})
 
-for pres in result:
-    for pressata in pres["steps"]:
+for riduttore in result:
+    for pressata in riduttore["steps"]:
         for id in ids:
             if pressata["id"]==id:
-                sample_rates[id].append(mean_sample_rate(pressata["altezza"]))
+                max_value = max(pressata["altezza"])
+                max_index = pressata["altezza"].index(max_value)
+                if max_index==0:
+                    print(pressata["id"])
+
+                sample_rates[id].append(mean_sample_rate(pressata["altezza"],max_index))
 
 for elem in sample_rates.items():
     print(elem[0],mean(elem[1]))
