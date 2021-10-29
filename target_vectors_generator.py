e211b1b15512465063c4a74a200bcf4a84a12804
giorgio289 <giorgio.scarton@studenti.unitn.it>
2021-10-29 17:44:58 +0200

    added debugging

--- target_vectors_generator.py
+++ target_vectors_generator.py
@@ -1,6 +1,8 @@
 from utils import mongo_connect,mongo_disconnect
 from numpy import NaN, mean,arange
-import json
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 def mean_sample_rate(samples):
     tmp=[]
@@ -37,16 +39,19 @@ def to_json(comp_id,taglia,max_h,rate):
 db,client=mongo_connect()
 collection= db.test2
 db.target_vectors.drop()
+logging.debug("Dropped target vector collection")
 
 result= collection.find().distinct("steps.id")
 ids=[]
 for id in result:
     ids.append(id)
+logging.debug("Got ID list")
 
 result= collection.find().distinct("taglia")
 tgl=[]
 for taglia in result:
     tgl.append(taglia)
+logging.debug("Got tgl list")
 
 #print(ids)
 
@@ -58,9 +63,12 @@ for id in ids:
         sample_rates[id][taglia]={}
         sample_rates[id][taglia]["max_v"]=0
         sample_rates[id][taglia]["rate"]=[]
+logging.debug("Initialized dictionary")
 
 result= collection.find({},{"taglia":1,"steps.id":1,"steps.altezza":1,"steps.forza":1})
+logging.debug("Query DB")
 
+cnt=1
 for riduttore in result:
     taglia_r=riduttore["taglia"]
     for pressata in riduttore["steps"]:
@@ -70,19 +78,25 @@ for riduttore in result:
         if len(pressata["altezza"])>0:
             #sample_rates[pressata["id"]][taglia_r]["rate"].append(mean_sample_rate(pressata["altezza"]))
             sample_rates[pressata["id"]][taglia_r]["rate"].append(min(pressata["altezza"]))
+        else:
+            logging.debug("Empty height vector n: "+str(cnt))
+            cnt+=1
 
+logging.debug("Started min rate computation")
 for id in ids:
     for taglia in tgl:
         #sample_rates[id][taglia]["rate"]=mean(sample_rates[id][taglia]["rate"])
         if len(sample_rates[id][taglia]["rate"])>0:
-            sample_rates[id][taglia]["rate"]=min(sample_rates[id][taglia]["rate"])
+            sample_rates[id][taglia]["rate"]=min(sample_rates[id][taglia]["rate"])      
+logging.debug("Ended min rate computation")
 
-
+logging.debug("Started DB insertion")
 for comp in sample_rates.keys():
         for tg in sample_rates[comp].keys():
             if sample_rates[comp][tg]["max_v"] != 0:
                 res=to_json(comp,tg,sample_rates[comp][tg]["max_v"],sample_rates[comp][tg]["rate"])
                 db.target_vectors.insert_one(res)
+logging.debug("Completed DB insertion")
 
 # keep at end of query
 mongo_disconnect(client)
