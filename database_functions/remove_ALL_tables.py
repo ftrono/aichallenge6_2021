c3ab1a4720114df85f69eaa6920c9a793c822650
aambroo <ambrosini.matteo21@gmail.com>
2021-11-11 18:51:59 +0100

    TEST

---
+++ database_functions/remove_ALL_tables.py
@@ -0,0 +1,42 @@
+import pyodbc
+
+# use preferred connection method and parameters ---------------------------------
+driver = '{ODBC Driver 17 for SQL Server}'
+server = 'localhost'
+database = 'NovoticAI'
+username = 'sa'
+password = 'AIchallenge6'
+
+con_string = 'DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password
+#---------------------------------------------------------------------------------
+
+# open connection
+cnxn = pyodbc.connect(con_string)
+print("Connected to", server)
+
+cursor = cnxn.cursor()
+
+cursor.execute('DROP TABLE WarningDesc')
+cnxn.commit()
+cursor.execute('DROP TABLE Warnings')
+cnxn.commit()
+cursor.execute('DROP TABLE CombosData')
+cnxn.commit()
+cursor.execute('DROP TABLE PressateData')
+cnxn.commit()
+cursor.execute('DROP TABLE Pressate')
+cnxn.commit()
+cursor.execute('DROP TABLE Combos')
+cnxn.commit()
+cursor.execute('DROP TABLE Riduttori')
+cnxn.commit()
+
+
+
+
+
+
+
+# close cursor and connection
+cursor.close()
+cnxn.close()
