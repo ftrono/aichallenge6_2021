7ea8393028379d90668d7caa7a2910268e5bab48
ftrono <82730291+ftrono@users.noreply.github.com>
2021-11-12 16:18:57 +0100

    correct decimals, add populate_max

---
+++ database_functions/populate_max.py
@@ -0,0 +1,25 @@
+import os
+from db_connect import db_connect, db_disconnect
+
+#Populate MaxForza and MaxAltezza fields in Pressate table
+
+# open connection
+fields = ['Forza', 'Altezza']
+cnxn, cursor = db_connect()
+temp = {}
+
+for s in fields:
+    #query data:
+    cursor.execute('SELECT Timestamp, MAX('+s+') as Max'+s+' FROM PressateData GROUP BY TIMESTAMP')
+    for row in cursor.fetchall():
+        temp[row[0]] = float(row[1])
+    cursor.commit()
+    #set data:
+    for k in temp.keys():
+        cursor.execute('UPDATE Pressate SET Max'+s+' = ? WHERE Timestamp = ?', temp[k], k)
+        cursor.commit()
+    #reset:
+    temp = {}
+
+#close cursor and connection
+db_disconnect(cnxn, cursor)
