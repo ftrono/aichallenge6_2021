462d3d973689a177d28f13e5567cd503a5598d3e
ftrono <82730291+ftrono@users.noreply.github.com>
2021-11-14 16:06:11 +0100

    add stdma to set_target_ma()

--- training/set_targets.py
+++ training/set_targets.py
@@ -2,28 +2,37 @@ import sys
 sys.path.insert(0, './')
 from database_functions.db_connect import db_connect, db_disconnect
 
+#SET TARGET_MA/MF & STD_MA/MF TO COMBOS TABLES:
 
-# TODO: sigma thing, using write_warnings(timestamp, wid) da utils, con wid = 1
-
-#set target MA: max of max_altezza within combo:
+#set target MA: max & std of max_altezza within combo:
 def set_targets_ma():
     '''
     NOTE: requires 
     1. Retrieves max value of Altezza for each ComboID (FROM: table Pressate)
     2. Saves that value as TargetMA for the correspondent ComboID (TO: table Combos)
+    3. Repeats the process for the Stdev of MA
     '''
     # Connect
     conn, cursor = db_connect()
 
-    # Get max value of max_altezza
-    get_max_altezza = 'SELECT ComboID, MAX(MaxAltezza) FROM Pressate GROUP BY ComboID'
-    cursor.execute(get_max_altezza)
+    #1) TARGET VALUE OF MAX_ALTEZZA FOR COMBOS:
+    cursor.execute('SELECT ComboID, MAX(MaxAltezza) FROM Pressate GROUP BY ComboID')
     # Save query output
     list = cursor.fetchall()
     # Update current values to new values for TargetMA
     for (comboID, max_altezza) in list:
-        max_altezza = float(max_altezza) # (decimal.Decimal -> float)
-        cursor.execute("UPDATE Combos SET TargetMA = ? WHERE ComboID = ?", max_altezza, comboID) # updates values
+        # updates values (decimal.Decimal -> float)
+        cursor.execute("UPDATE Combos SET TargetMA = ? WHERE ComboID = ?", float(max_altezza), comboID)
+    cursor.commit()
+
+    #2) STDEV OF MAX_ALTEZZA FOR COMBOS:
+    cursor.execute('SELECT ComboID, STDEV(MaxAltezza) FROM Pressate GROUP BY ComboID')
+    #Save query output
+    list = cursor.fetchall()
+    #Update current values to new values for StdMA
+    for (comboID, std_ma) in list:
+        # updates values
+        cursor.execute("UPDATE Combos SET StdMA = ? WHERE ComboID = ?", float(std_ma), comboID) 
     cursor.commit()
 
     # Disconnect
@@ -38,6 +47,7 @@ def set_targets_mf():
     NOTE: requires 
     1. Retrieves average value of Forza for each ComboID (FROM: table Pressate)
     2. Saves that value as TargetMF for the correspondent ComboID (TO: table Combos)
+    3. Repeats the process for the Stdev of MF
     '''
     # Connect
     conn, cursor = db_connect()
@@ -48,7 +58,8 @@ def set_targets_mf():
     list = cursor.fetchall()
     #Update current values to new values for TargetMF
     for (comboID, max_forza) in list:
-        cursor.execute("UPDATE Combos SET TargetMF = ? WHERE ComboID = ?", float(max_forza), comboID) # updates values
+        # updates values
+        cursor.execute("UPDATE Combos SET TargetMF = ? WHERE ComboID = ?", float(max_forza), comboID) 
     cursor.commit()
 
     #2) STDEV OF MAX_FORZA FOR COMBOS:
@@ -57,7 +68,8 @@ def set_targets_mf():
     list = cursor.fetchall()
     #Update current values to new values for StdMF
     for (comboID, std_mf) in list:
-        cursor.execute("UPDATE Combos SET StdMF = ? WHERE ComboID = ?", float(std_mf), comboID) # updates values
+        # updates values
+        cursor.execute("UPDATE Combos SET StdMF = ? WHERE ComboID = ?", float(std_mf), comboID)
     cursor.commit()
 
     # Disconnect
@@ -67,5 +79,5 @@ def set_targets_mf():
 
 
 #MAIN:
-#set_targets_ma()
+set_targets_ma()
 #set_targets_mf()
