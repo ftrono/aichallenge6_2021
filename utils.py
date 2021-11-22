16d2b832e87075836201094fc81b0b42477139ae
aichallengeTeam6 <team6.aichallenge@gmail.com>
2021-11-22 17:47:26 +0000

    chore new interpolate

--- utils.py
+++ utils.py
@@ -1,8 +1,10 @@
 import sys, os, warnings, pyodbc
+from scipy import interpolate
 import matplotlib.pyplot as plt
 sys.path.insert(0, os.getcwd())
 from globals import *
 from database_functions.db_connect import db_connect, db_disconnect
+
 
 #COMMON UTILITY FUNCTIONS:
 # - execute_query_file()
@@ -97,76 +99,92 @@ def visualize(current, target, wid=0, count_out=0, window=WINDOW, save=SAVE_PNG)
     return 0
 
 
-#HORIZONTAL INTERPOLATION:
-#interpolate curve based on target altezza vector for the combo:
-def interpolate_curve(altezza_combo, altezza, forza):
-    '''
-    Aligns horizontally both the forza and altezza vectors on the basis of a "stamp",
-    represented by the target altezza vector for a combo(taglia, id_comp).
-    The target altezza vector has a standard number of points based on a predefined sample rate.
+# #HORIZONTAL INTERPOLATION:
+# #interpolate curve based on target altezza vector for the combo:
+# def interpolate_curve(altezza_combo, altezza, forza):
+#     '''
+#     Aligns horizontally both the forza and altezza vectors on the basis of a "stamp",
+#     represented by the target altezza vector for a combo(taglia, id_comp).
+#     The target altezza vector has a standard number of points based on a predefined sample rate.
 
-    params: (list) target vector for a combo(taglia, altezza):,
-            (list) orig altezza vector:,
-            (list) orig forza vector.
-    returns:
-            (list) interpolated forza vector.
-    '''
-    #objects:
-    new_forza = []
-    i = 0
-    v = 0
-    tgt_len = len(altezza_combo)
+#     params: (list) target vector for a combo(taglia, altezza):,
+#             (list) orig altezza vector:,
+#             (list) orig forza vector.
+#     returns:
+#             (list) interpolated forza vector.
+#     '''
+#     #objects:
+#     new_forza = []
+#     i = 0
+#     v = 0
+#     tgt_len = len(altezza_combo)
 
-    if altezza == [] or altezza_combo == []:
-        return warnings.warn("Error: empty list.")
+#     if altezza == [] or altezza_combo == []:
+#         return warnings.warn("Error: empty list.")
 
-    #1) initial padding:
-    if altezza[0] > altezza_combo[0]:
-        while altezza_combo[i] < altezza[0]:
-            #pad:
-            new_forza.append(0)
-            i = i+1
+#     #1) initial padding:
+#     if altezza[0] > altezza_combo[0]:
+#         while altezza_combo[i] < altezza[0]:
+#             #pad:
+#             new_forza.append(0)
+#             i = i+1
 
-    #2) interpolate values:
-    for i in range(i, tgt_len):
-        #pad check:
-        if v < len(altezza):
-            #find closest value of altezza_combo[i] in orig altezza list:
-            #the loop will stop at the last index (v) in which the value of altezza is before (or equal to) the value of altezza_combo[i]:
-            while (altezza[v] < altezza_combo[i]):
-                #go to next:
-                v = v+1
-                #check end of orig list:
-                if v >= len(altezza):
-                    #ending padding:
-                    new_forza.append(0)
-                    break
+#     #2) interpolate values:
+#     for i in range(i, tgt_len):
+#         #pad check:
+#         if v < len(altezza):
+#             #find closest value of altezza_combo[i] in orig altezza list:
+#             #the loop will stop at the last index (v) in which the value of altezza is before (or equal to) the value of altezza_combo[i]:
+#             while (altezza[v] < altezza_combo[i]):
+#                 #go to next:
+#                 v = v+1
+#                 #check end of orig list:
+#                 if v >= len(altezza):
+#                     #ending padding:
+#                     new_forza.append(0)
+#                     break
             
-            if v < len(altezza):
-                #calculate interpolated value:
-                if altezza[v] == altezza_combo[i]:
-                    #if equal, save same:
-                    new_forza.append(forza[v])
-                else:
-                    #INTERPOLATE:
-                    #indices:
-                    left = v-1
-                    right = v
+#             if v < len(altezza):
+#                 #calculate interpolated value:
+#                 if altezza[v] == altezza_combo[i]:
+#                     #if equal, save same:
+#                     new_forza.append(forza[v])
+#                 else:
+#                     #INTERPOLATE:
+#                     #indices:
+#                     left = v-1
+#                     right = v
 
-                    dx = altezza[right]-altezza[left]
-                    xgap = altezza[right]-altezza_combo[i]
-                    dy = forza[right]-forza[left]
-                    #xgap:dx=ygap:dy
-                    ygap = (xgap*dy)/dx
+#                     dx = altezza[right]-altezza[left]
+#                     xgap = altezza[right]-altezza_combo[i]
+#                     dy = forza[right]-forza[left]
+#                     #xgap:dx=ygap:dy
+#                     ygap = (xgap*dy)/dx
 
-                    #calculate:
-                    new_forza.append(round((forza[right]-ygap),2))
+#                     #calculate:
+#                     new_forza.append(round((forza[right]-ygap),2))
                     
-                    #print("Altezza: {}, {}, {}".format(altezza[left], altezza[right], altezza_combo[i]))
-                    #print("Forza: {}, {}, {}".format(forza[left], forza[right], new_forza[i]))
+#                     #print("Altezza: {}, {}, {}".format(altezza[left], altezza[right], altezza_combo[i]))
+#                     #print("Forza: {}, {}, {}".format(forza[left], forza[right], new_forza[i]))
         
-        else:
-            #3) Ending padding:
-            new_forza.append(0)
+#         else:
+#             #3) Ending padding:
+#             new_forza.append(0)
 
+#     return new_forza
+
+def interpolate_curve(altezza_combo, altezza, forza):
+    def f(x):
+        x_points = altezza
+        y_points = forza
+
+        tck = interpolate.splrep(x_points, y_points)
+        return interpolate.splev(x, tck)
+    
+    new_forza=[]
+    for point in altezza_combo:
+        if point<altezza[0]:
+            new_forza.append(0)
+        else:
+            new_forza.append(f(point))
     return new_forza
