6d9bf6044522c58758589bc2135cb9b6f7bce35d
aambroo <ambrosini.matteo21@gmail.com>
2021-11-23 18:11:20 +0100

    read_sql now works

--- database_functions/report_generator.py
+++ database_functions/report_generator.py
@@ -5,10 +5,20 @@ import pandas as pd
 # connect
 conn, cursor = db_connect()
 
-query = ("SELECT Pressate.RiduttoreID, Pressate.ComboID, Pressate.MaxForza, Pressate.MaxAltezza, Warnings.WarningID FROM (Pressate INNER JOIN Warning ON Warnings.RiduttoreID = Pressate.RiduttoreID)")
-
+query = ("SELECT Pressate.RiduttoreID, Pressate.ComboID, Pressate.MaxForza, Pressate.MaxAltezza, Warnings.WarningID FROM (Pressate INNER JOIN Warnings ON Warnings.RiduttoreID = Pressate.RiduttoreID)")
+# save query as dataframe
+df = pd.read_sql(query, conn)
+# 1. raggruppare per riduttore
+# 2. creare csv (per ciascun riduttore)
+# 3. inserire dati raggruppati per combo
+# 4. | Combo | ... | Warning | Warning_description |
+#    -----------------------------------------------
+#    |       | ... |         |                     |
+#    |       | ... |         |                     |
+#    |       | ... |         |                     |
+print(df)
 
 
 
 # disconnect
-db_connect(conn, cursor)
+db_disconnect(conn, cursor)
