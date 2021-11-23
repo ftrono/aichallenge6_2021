ee66007621607b530d3cf91e418e99a5bb6df124
aambroo <ambrosini.matteo21@gmail.com>
2021-11-23 17:11:09 +0100

    report generator function (beginning)

---
+++ database_functions/report_generator.py
@@ -0,0 +1,13 @@
+from database_functions.db_connect import db_connect, db_disconnect
+from export.export_to_csv import save_table_csv
+import pandas as pd
+
+# connect
+conn, cursor = db_connect()
+
+query = ("SELECT Pressate.RiduttoreID, Pressate.ComboID, Pressate.MaxForza, Pressate.MaxAltezza, Warnings.WarningID FROM (Pressate INNER JOIN Warning ON Warnings.RiduttoreID = Pressate.RiduttoreID)")
+
+
+
+# disconnect
+db_connect(conn, cursor)
