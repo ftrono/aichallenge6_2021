7fdbafb98d1651a06dead8d8a9917b682a2f77ca
eliana97 <eliana.battisti@gmail.com>
2021-11-28 10:59:47 +0100

    Modified save_table_warnings()

--- export/export_to_csv.py
+++ export/export_to_csv.py
@@ -33,7 +33,7 @@ def save_table_csv(table_name):
     
     return 0
 
-def save_table_warnings(combo_id):
+def save_table_warnings(combo_id, timestamp, riduttore_id,wid,stazione):
     '''
     Function that save a table warnings of db in a file with extension as 'formato'. '
     The file is located in OUTPUT_PATH with name "combo_id"+Warning.csv.
@@ -42,19 +42,58 @@ def save_table_warnings(combo_id):
 
     Input:
     - combo_id :  string
+    - timestamp: string
+    - riduttore_id: string
+    - wid (warningID): string
+    - stazione  : string
     
 
     '''
     conn,cursor = db_connect()
-    sql_query = pd.read_sql_query("SELECT Pressate.Timestamp, Pressate.ComboID, Warnings.WarningID FROM Pressate INNER JOIN Warnings ON (Warnings.Timestamp = Pressate.Timestamp) WHERE Pressate.ComboID='"+str(combo_id)+"'",conn)
-    df = pd.DataFrame(sql_query)
-    if not os.path.isdir(OUTPUT_PATH):
-        os.mkdir(OUTPUT_PATH)
-    percorso= os.path.join(OUTPUT_PATH,combo_id +"Warning"+'.'+"csv")
-    df.to_csv(percorso,index=False)    
+    if (combo_id is not None) and (timestamp=="") and (riduttore_id=="") and (wid=="") and (stazione==""):
+        sql_query = pd.read_sql_query("SELECT Pressate.Timestamp, Pressate.RiduttoreID, Pressate.ComboID, Pressate.Stazione, Warnings.WarningID FROM Pressate INNER JOIN Warnings ON (Warnings.Timestamp = Pressate.Timestamp) WHERE Pressate.ComboID='"+str(combo_id)+"'",conn)
+        df = pd.DataFrame(sql_query)
+        if not os.path.isdir(OUTPUT_PATH):
+            os.mkdir(OUTPUT_PATH)
+        percorso= os.path.join(OUTPUT_PATH,combo_id +"WarningELIANAPROVA"+'.'+"csv")
+        df.to_csv(percorso,index=False)
+
+    elif (combo_id=="") and (timestamp is not None) and (riduttore_id=="") and (wid=="")and (stazione==""):
+        sql_query = pd.read_sql_query("SELECT  Pressate.Timestamp, Pressate.RiduttoreID, Pressate.ComboID, Pressate.Timestamp, Pressate.Stazione, Warnings.WarningID FROM Pressate INNER JOIN Warnings ON (Warnings.Timestamp = Pressate.Timestamp) WHERE Warnings.Timestamp='"+str(timestamp)+"'",conn)
+        df = pd.DataFrame(sql_query)
+        if not os.path.isdir(OUTPUT_PATH):
+            os.mkdir(OUTPUT_PATH)
+        percorso= os.path.join(OUTPUT_PATH, timestamp +"Warning"+'.'+"csv")
+        df.to_csv(percorso,index=False)
+
+    elif (combo_id=="") and (timestamp=="") and (riduttore_id is not None) and (wid=="")and (stazione==""):
+        sql_query = pd.read_sql_query("SELECT  Pressate.Timestamp, Pressate.RiduttoreID, Pressate.ComboID, Pressate.Timestamp, Pressate.Stazione, Warnings.WarningID FROM Pressate INNER JOIN Warnings ON (Warnings.Timestamp = Pressate.Timestamp) WHERE Pressate.RiduttoreID='"+str(riduttore_id)+"'",conn)
+        df = pd.DataFrame(sql_query)
+        if not os.path.isdir(OUTPUT_PATH):
+            os.mkdir(OUTPUT_PATH)
+        percorso= os.path.join(OUTPUT_PATH,riduttore_id +"Warning"+'.'+"csv")
+        df.to_csv(percorso,index=False)
+
+    elif (combo_id=="") and (timestamp=="") and (riduttore_id=="") and (wid is not None) and (stazione==""):
+        sql_query = pd.read_sql_query("SELECT  Pressate.Timestamp, Pressate.RiduttoreID, Pressate.ComboID, Pressate.Timestamp, Pressate.Stazione, Warnings.WarningID FROM Pressate INNER JOIN Warnings ON (Warnings.Timestamp = Pressate.Timestamp) WHERE Warnings.WarningID='"+str(wid)+"'",conn)
+        df = pd.DataFrame(sql_query)
+        if not os.path.isdir(OUTPUT_PATH):
+            os.mkdir(OUTPUT_PATH)
+        percorso= os.path.join(OUTPUT_PATH,wid +"Warning"+'.'+"csv")
+        df.to_csv(percorso,index=False)
+
+    elif (combo_id=="") and (timestamp=="") and (riduttore_id=="") and (wid=="") and (stazione is not None):
+        sql_query = pd.read_sql_query("SELECT  Pressate.Timestamp, Pressate.RiduttoreID, Pressate.ComboID, Pressate.Timestamp, Pressate.Stazione, Warnings.WarningID FROM Pressate INNER JOIN Warnings ON (Warnings.Timestamp = Pressate.Timestamp) WHERE Pressate.Stazione='"+str(stazione)+"'",conn)
+        df = pd.DataFrame(sql_query)
+        if not os.path.isdir(OUTPUT_PATH):
+            os.mkdir(OUTPUT_PATH)
+        percorso= os.path.join(OUTPUT_PATH,stazione +"Warning"+'.'+"csv")
+        df.to_csv(percorso,index=False)
+ 
+    
     db_disconnect(conn,cursor)
 
 
     return 0
 
-#save_table_warnings("a0215MP080")
+#save_table_warnings("","1584106890","","","")
