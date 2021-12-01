08d656df315cbb1ba3ce8699130fb3ae2b2e6c0f
giorgio289 <giorgio.scarton@studenti.unitn.it>
2021-12-01 11:04:22 +0100

    add check

--- database_functions/insert_data.py
+++ database_functions/insert_data.py
@@ -23,6 +23,8 @@ def parse_date(cell):
     return timestamp
 
 def generate_ComboID(idComp,taglia,stazione,master,rapporto,stadi):
+    if idComp=='p0009':
+        print("id: {},stazione: {}, taglia: {}, master: {}".format(idComp,stazione,taglia,master))
     p009_rap=[30,40,70,100]
     if idComp=='p0009' and stazione=='a1' and master==2 and (rapporto in p009_rap):
         return idComp+taglia+'i10'
